571327250c8bae1a4fe5ae3712330ac4bbaa8ff3
JJ0ng <2000jiyong@gmail.com>
2023-04-06 22:14:17 +0900

    간단한 JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,30 +4,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        //하나만 생성해서 애플리케이션 전체에서 공유
+        //JPA는 EntityManagerFactroy를 만들어야 한다. 데이터베이스 당 하나만 생성해서 애플리케이션 전체에서 공유
 
-        EntityManager em = emf.createEntityManager(); //쓰레드간에 공유X, 사용하고 버려야 함
+        EntityManager em = emf.createEntityManager(); //쓰레드간에 공유X, 사용하고 버려야 함(요청)
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try{
-            //조회
-            Member findMember = em.find(Member.class, 1L); // 1L -> pk
-            //수정, em.persist 할 필요가 X, 커밋시점에서 감지 -> 자바 컬렉션처럼 동작
-            findMember.setName("Jiyong");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class) //대상이 엔티티 객체(테이블X)
+                    .setFirstResult(1)
+                    .setMaxResults(10) //1~10
+                    .getResultList();
 
-            //삭제
-            //em.remove(findMember);
+            for(Member member : result){
+                System.out.println("member.name = " + member.getName());
+            }
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
-        }finally {
-            em.close(); //예외처리가 중요하다
+        }finally { //예외처리가 중요하다
+            em.close(); //닫아줘야 반환이 된다
         }
 
         em.close();
