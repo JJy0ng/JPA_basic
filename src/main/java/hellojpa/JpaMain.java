bccfc19bbf46556a292f1181c872d76f702b615d
JJ0ng <2000jiyong@gmail.com>
2023-04-09 23:40:21 +0900

    영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        //JPA는 EntityManagerFactroy를 만들어야 한다. 데이터베이스 당 하나만 생성해서 애플리케이션 전체에서 공유
+        //JPA는 EntityManagerFactory를 만들어야 한다. 데이터베이스 당 하나만 생성해서 애플리케이션 전체에서 공유
 
         EntityManager em = emf.createEntityManager(); //쓰레드간에 공유X, 사용하고 버려야 함(요청)
 
@@ -17,20 +17,22 @@ public class JpaMain {
         tx.begin();
 
         try{
-            List<Member> result = em.createQuery("select m from Member as m", Member.class) //대상이 엔티티 객체(테이블X)
-                    .setFirstResult(1)
-                    .setMaxResults(10) //1~10
-                    .getResultList();
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJpa");
 
-            for(Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member); //엔티티매니저 안에 있는 영속성 컨텍스트 통해 멤버 객체가 관리가 된다, db에 저장x
+            //em.detach(member); 멤버를 영속성 컨텍스트에서 분리, 준영속 상태
+            System.out.println("=== AFTER ===");
 
-            tx.commit();
+            tx.commit(); //여기서 db에 쿼리가 나감
         }catch (Exception e){
             tx.rollback();
-        }finally { //예외처리가 중요하다
-            em.close(); //닫아줘야 반환이 된다
+        }finally {
+            em.close();
         }
 
         em.close();
